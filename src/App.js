a2ed3d5bf1e0937a2305936991863f40c4ce3336
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-10-16 15:43:05 +0800

    search bar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <SearchBar />
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return (
+        <div className='max-w-7xl mx-auto px-12 flex justify-center mt-5'>
+            <div>
+                <div>
+                    <h2 className='text-3xl text-center'>The Twelve Cafe Bar Recipe</h2>
+                </div>
+                <div className="form-control mx-auto mt-5">
+                    <div className="input-group w-5xl">
+                        <input type="text" placeholder="Search…" className="input input-bordered max-w-5xl" />
+                        <button className="btn btn-square">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SearchBar;
